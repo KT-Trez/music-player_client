67d6351a5b3fc7e89ca479f356f88b8354017eed
Trez <63346093+KT-Trez@users.noreply.github.com>
2022-06-19 17:34:32 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import AppRoot from './AppRoot';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './css/master.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppRoot/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
